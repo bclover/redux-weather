50a5548316ea3ee4ee448a643164fe40b9d07c50
Bryan Clover <bclover09@gmail.com>
2016-09-19 16:21:35 -0400

    Completed thru Section 5, Lecture 57'

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'c7d8653c13f19abb586398021fe13cb3';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const COUNTRY = 'us';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},${COUNTRY}`;
+  const request = axios.get(url);
+  
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
